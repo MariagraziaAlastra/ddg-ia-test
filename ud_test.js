e37b6f3a7f03d075cb226a42ef4674a25d143b55
MariagraziaAlastra <alastra.mariagrazia@gmail.com>
2014-09-15 17:05:53 +0200

    Add some tests comparing IA content to the API result

--- ud_test.js
+++ ud_test.js
@@ -1,5 +1,6 @@
 casper.test.begin('Urban Dictionary IA is correctly shown', function suite(test) {
     var query = "ud rad";
+    var api_url = "http://api.urbandictionary.com/v0/define?term=rad";
 
     casper.start("https://bttf.duckduckgo.com", function() {
         test.assertTitle("DuckDuckGo", "DDG title is the one expected");
@@ -27,7 +28,26 @@ casper.test.begin('Urban Dictionary IA is correctly shown', function suite(test)
         // Check if the Infobox elements exist
         test.assertExists('div.zci--urban_dictionary h6.info--head', "Infobox title exists");
         test.assertExists('div.zci--urban_dictionary div.info span.info__label a', "related words exist");
-});
+    });
+
+    casper.then(function() {
+        var data = this.evaluate(function(api_url) {
+            return JSON.parse(__utils__.sendAJAX(api_url));
+        }, {api_url: api_url});
+
+        // Check if all the elements have the right values from the API response
+        header_text =  this.fetchText('div.zci--urban_dictionary h3 span.zci__urban__word');
+        test.assertEquals(header_text, data.list[0].word, "title equals the searched word");
+
+        definition_text =  this.fetchText('div.zci--urban_dictionary p.ud_definition').trim();
+        definition_text = definition_text.replace("\n", "");
+        test.assertEquals(definition_text, data.list[0].definition, "the definition is the right one");
+
+        example_text =  this.fetchText('div.zci--urban_dictionary div.ud_example div.example').trim();
+        example_text = example_text.replace(/\r?\n/gi, "");
+        data.list[0].example = data.list[0].example.replace(/\r?\n/gi, "");
+        test.assertEquals(example_text, data.list[0].example, "the example is the right one");
+    });
 
     casper.run(function() {
         test.done();
