e712b86ec6819db307ef944645054cabbb9a9101
MariagraziaAlastra <alastra.mariagrazia@gmail.com>
2014-09-25 12:24:46 +0200

    Add kwixer.json

--- tiles.js
+++ tiles.js
@@ -1,6 +1,6 @@
 casper.test.begin('IAs with tiles are correctly shown', function suite(test) {
     // This is just for testing - the path should actually be passed as a command-line arg
-    var path = "./json/tiles/in_theaters.json";
+    var path = "./json/tiles/kwixer.json";
     var data = require(path);
     var metabar_regex = /^Showing\s[0-9]+\s([A-Za-z]+\+?|[A-Z])(\s|\.)(([A-Za-z]+|[A-Z])(\s|\.))*for(\s([A-Za-z]+|[A-Z]))*$/;
     var moreAt_regex = new RegExp(data.moreAt_regex);
@@ -302,7 +302,7 @@ casper.test.begin('IAs with tiles are correctly shown', function suite(test) {
                 test.assertEquals(detail_title, tile_title, "detail title matches selected tile title");
             } else if (detail_title.length > tile_title.length) {
                 test.assertEquals(tile_title.substr(-3, 3), "...", "selected tile title has ellipsis");
-                test.assertEquals(tile_title.substring(0, -3), detail_title.substr(0, tile_title.length - 3),
+                test.assertEquals(tile_title.substr(0, tile_title.length - 3), detail_title.substr(0, tile_title.length - 3),
                                  "detail title matches selected tile title");
             } else {
                 test.fail("detail title is different from selected tile title");
