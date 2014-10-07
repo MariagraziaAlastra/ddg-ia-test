6cdc8a8dfa458e4af2cbeff0c32d26ad3f04badf
MariagraziaAlastra <alastra.mariagrazia@gmail.com>
2014-10-07 17:06:17 +0200

    test_existence.js - More descriptive test comments

--- test_existence.js
+++ test_existence.js
@@ -35,37 +35,43 @@ module.exports = function(path) {
      * expected_selectors -> object containing default and optional selectors for a specific template group
      */
     function checkSelectors(root, elements, expected_selectors) {
+        var error_msg = "";
         for(var key in elements) {
             if(elements[key] !== null && key !== 'root') {
                 if((expected_selectors.
                     default.indexOf(key) !== -1) || ((expected_selectors.optional.indexOf(key) !== -1) && data['has_' + key])) {
                     // Leaving this here for debug purposes
                     casper.echo("Expected: " + key);
+                    error_msg = " - Expected selector " + key + " does not exist";
                     if(typeof elements[key] === 'object') {
-                        if(!(casper.exists(root + " " + elements[key].root) && checkSelectors(elements[key].root, elements[key], expected_selectors))) {
-                            return false;
+                        var recursive_result = checkSelectors(elements[key].root, elements[key], expected_selectors);
+                        recursive_result.msg = (recursive_result.msg !== null)? recursive_result.msg : error_msg;
+                        if(!(casper.exists(root + " " + elements[key].root) && recursive_result.pass)) {
+                            return {"pass" : false, "msg" : recursive_result.msg};
                         }
                     } else if(typeof elements[key] === 'string') {
                         if(!casper.exists(root + " " + elements[key])) {
-                            return false;
+                            return {"pass" : false, "msg" : error_msg};
                         }
                     }
                 } else {
                     // Leaving this here for debug purposes
                     casper.echo("Unexpected: " + key);
+                    error_msg = " - Unexpected selector " + key + " exists";
                     if(typeof elements[key] === 'object') {
-                        if(casper.exists(root + " " + elements[key].root)) {
-                            return false;
+                        if(casper.exists(root_selectors.main + " " + root + " " + elements[key].root)) {
+                            return {"pass" : false, "msg" : error_msg};
                         }
                     } else if(typeof elements[key] === 'string') {
                         if(casper.exists(root + " " + elements[key])) {
-                            return false;
+                            casper.test.comment(error_msg);
+                            return {"pass" : false, "msg" : error_msg};
                         }
                     }
                 }
             }
         }
-        return true;
+        return {"pass" : true, "msg" : ""};
     }
 
     casper.test.assertExists(root_selectors.ia_tab, data.name + " IA is shown");
@@ -85,18 +91,12 @@ module.exports = function(path) {
         };
 
         if(template_group.cw) {
-            custom_cw.
-            default = all_groups.cw.
-            default.concat(template_group.cw.
-                default);
+            custom_cw.default = all_groups.cw.default.concat(template_group.cw.default);
             custom_cw.optional = all_groups.cw.optional.concat(template_group.cw.optional);
         }
 
         if(template_group.aux) {
-            custom_aux.
-            default = all_groups.aux.
-            default.concat(template_group.aux.
-                default);
+            custom_aux.default = all_groups.aux.default.concat(template_group.aux.default);
             custom_aux.optional = all_groups.aux.optional.concat(template_group.aux.optional);
         }
 
@@ -106,14 +106,16 @@ module.exports = function(path) {
         casper.test.assertExists((root_selectors.main + " " + root_selectors.no_tiles.content), "Content exists");
 
         casper.test.comment("Check content selectors")
-        casper.test.assert(checkSelectors(root_selectors.no_tiles.content, cw_selectors, custom_cw), "Content's nested elements exist");
+        var content_result = checkSelectors(root_selectors.no_tiles.content, cw_selectors, custom_cw);
+        casper.test.assert(content_result.pass, "Content's nested elements exist" + content_result.msg);
 
         if(data.has_aux) {
             casper.test.comment("Check if Infobox exists");
             casper.test.assertExists((root_selectors.main + " " + root_selectors.no_tiles.aux), "Infobox exists");
 
             casper.test.comment("Check Infobox selectors")
-            casper.test.assert(checkSelectors(root_selectors.no_tiles.aux, aux_selectors, custom_aux), "Infobox's nested elements exist");
+            var infobox_result = checkSelectors(root_selectors.no_tiles.aux, aux_selectors, custom_aux);
+            casper.test.assert(infobox_result.pass, "Infobox's nested elements exist" + infobox_result.msg);
         }
     } else {
         var custom_tiles = {
@@ -126,18 +128,12 @@ module.exports = function(path) {
         };
 
         if(template_group.tiles) {
-            custom_tiles.
-            default = all_groups.tiles.
-            default.concat(template_group.tiles.
-                default);
+            custom_tiles.default = all_groups.tiles.default.concat(template_group.tiles.default);
             custom_tiles.optional = all_groups.tiles.optional.concat(template_group.tiles.optional);
         }
 
         if(template_group.detail) {
-            custom_detail_after.
-            default = all_groups.detail_after.
-            default.concat(template_group.detail.
-                default);
+            custom_detail_after.default = all_groups.detail_after.default.concat(template_group.detail.default);
             custom_detail_after.optional = all_groups.detail_after.optional.concat(template_group.detail.optional);
         }
 
@@ -147,16 +143,17 @@ module.exports = function(path) {
 
         casper.test.comment("Check if metabar exists");
         casper.test.assertExists((root_selectors.main + " " + root_selectors.tiles.metabar), "Metabar exists");
+
         casper.test.comment("Check if metabar contains the expected nested elements");
-        casper.test.assert(checkSelectors(root_selectors.tiles.metabar, metabar_selectors, all_groups.metabar),
-                          "Metabar's expected nested elements exist");
+        var metabar_result = checkSelectors(root_selectors.tiles.metabar, metabar_selectors, all_groups.metabar);
+        casper.test.assert(metabar_result.pass, "Metabar's expected nested elements exist" + metabar_result.msg);
 
         casper.test.comment("Check if tiles exist");
         casper.test.assertExists((root_selectors.main + " " + root_selectors.tiles.tiles), "Tiles exist");
 
         casper.test.comment("Check if tiles contains the expected nested elements");
-        casper.test.assert(checkSelectors(root_selectors.tiles.tiles, tiles_selectors, custom_tiles),
-                          "Tiles expected nested elements exist");
+        var tiles_result = checkSelectors(root_selectors.tiles.tiles, tiles_selectors, custom_tiles);
+        casper.test.assert(tiles_result.pass, ("Tiles expected nested elements exist" + tiles_result.msg));
 
         casper.test.comment("Check that no tile is selected right after page loading");
         casper.test.assertDoesntExist((root_selectors.main + " " + tiles_selectors.tile.root + class_selected), "No tile is selected");
@@ -165,8 +162,8 @@ module.exports = function(path) {
             casper.test.assertExists((root_selectors.main + " " + root_selectors.tiles.detail), "Detail exists");
 
             casper.test.comment("Check if detail contains the expected nested elements and its content is empty");
-            casper.test.assert(checkSelectors(root_selectors.tiles.detail, detail_selectors, all_groups.detail_before),
-                              "Detail's expected nested elements exist");
+            var detail_result = checkSelectors(root_selectors.tiles.detail, detail_selectors, all_groups.detail_before);
+            casper.test.assert(detail_result.pass, "Detail's expected nested elements exist" + detail_result.msg);
 
             casper.test.comment("Select tile and see if detail content appears");
             casper.click(root_selectors.main + " " + tiles_selectors.tile.root);
